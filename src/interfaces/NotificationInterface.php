e0121dcb858e33a94a19ec369310daf4a104ceb8
floor12 <floor12@floor12.net>
2019-02-06 12:07:09 +0100

    Start of creating main model prototype.

---
+++ src/interfaces/NotificationInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: floor12
+ * Date: 2019-02-06
+ * Time: 12:03
+ */
+
+namespace floor12\notifications\interfaces;
+
+interface NotificationInterface
+{
+
+}
---
+++ src/models/Notification.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: floor12
+ * Date: 2019-02-06
+ * Time: 12:06
+ */
+
+namespace floor12\notifications\models;
+
+
+class Notification
+{
+
+}
